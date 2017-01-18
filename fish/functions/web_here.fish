f7edece65356e535b266ab31022334ada1542e83
Máté Farkas <mate.farkas@balabit.com>
2017-01-18 13:02:17 +0100

    web_here: make URL easily selectable
    In terminal it is easier to select it when the line contains
    only the target text of selection.
    
    Signed-off-by: Máté Farkas <mate.farkas@balabit.com>

--- fish/functions/web_here.fish
+++ fish/functions/web_here.fish
@@ -1,15 +1,18 @@
 function web_here
-    set -l ip_addresses (ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | grep -v 172.)
+	set -l ip_addresses (ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | grep -v 172.)
     set -l port (math (random)"+1024")
 
-    set_color green; echo -n "Serving directory "
-    set_color magenta; echo -n (string trim (pwd))"/"
-    set_color green; echo " on the following addresses:"
+    set_color green
+    echo -n "Serving directory "
+    set_color magenta
+    echo -n (string trim (pwd))"/"
+    set_color green
+    echo " on the following addresses:"
     echo
 
     set_color yellow
     for ip_address in $ip_addresses
-        echo -e "\t- http://"$ip_address":"$port"/"
+        echo -e "\thttp://"$ip_address":"$port"/"
     end
     echo
 
